1d4ba534061d48db17c1364371e46e72e9735739
sonia <sonia.ferkous@gmail.com>
2019-05-02 10:19:50 +0200

    mise à jour

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,32 @@
-test
+$(document).ready(function(){
+
+  $('.card-deck').hide();
+
+  $('#btnAccueil').click(function(){
+    $('.elem').hide();
+    $('.carousel').show()
+  });
+
+  $('#btnBehourd').click(function(){
+    $('.elem').hide();
+    $('.prodBehourd').show()
+  });
+
+  $('#btnFant').click(function(){
+    $('.elem').hide();
+    $('.prodFant').show()
+  });
+
+  $('#btnLaser').click(function(){
+    $('.elem').hide();
+    $('.prodLaser').show()
+  });
+
+  $('#btnSupport').click(function(){
+    $('.elem').hide();
+    $('.prodSupport').show()
+  });
+
+
+
+});
