7d64340b24b37c3272e6a9ef6e2a1771f5ae63c4
sonia <sonia.ferkous@gmail.com>
2019-05-02 14:39:40 +0200

    mise à jour

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,22 @@ $(document).ready(function(){
     $('.prodSupport').show()
   });
 
+  $('.card').click(function(){
+
+    var article = [];
+    article[0] = $(this).find('.id').text();      //Identifiant
+    article[1] = $(this).find('.name').text();    //Nom produit
+    article[2] = $(this).find('img').attr('src'); //Image produit
+    article[3] = $(this).find('.descL').text();   //Description longue
+    article[4] = $(this).find('.price').text();   //Prix produit
+    console.log(article);
+
+    $('.modal-title').text(article[1]);
+    $('.modal').find('img').attr('src', article[2]);
+    $('.descLM').text(article[3]);
+    $('.priceM').text(article[4]);
+
+});
   $('#btnContact').click(function(){
     $('.elem').hide();
     $('.Contact').show()
